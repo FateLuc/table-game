441c60b4e4d3cc05e7064e18a6df9244d40bf47a
Feger <luc.feger@bluewin.ch>
2019-11-27 16:10:02 +0100

    Images

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+        <table>
+            <?php for ($i=0;$i<10;$i++){ ?>
+            <tr>
+                <?php for ($i=0;$i<10;$i++){ ?>
+                <td>
+                    <img src="Download.png" weidth="2.5px" length="2.5px"
+                </td>
+                <?php } ?>
+            </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>
