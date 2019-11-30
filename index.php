2bba66c761b3bed1fa8c26551e279c1f911333b6
FateLuc <luc.feger@bluewin.ch>
2019-11-30 17:42:43 +0100

    11x11 field add

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
     <body>
         <table>
-            <?php for ($i=0;$i<10;$i++){ ?>
+            <?php for ($u=0;$u<11;$u++){ ?>
             <tr>
-                <?php for ($i=0;$i<10;$i++){ ?>
+                <?php for ($i=0;$i<11;$i++){ ?>
                 <td>
-                    <img src="Download.png" weidth="2.5px" length="2.5px"
+                    <img src="Download.png" weidth="1px" length="1px"
                 </td>
                 <?php } ?>
             </tr>
