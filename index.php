ce1a296b4fe873f4b4b8c66440169532e26dfc8a
FateLuc <luc.feger@bluewin.ch>
2019-12-04 10:08:04 +0100

    add green field

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$x=$_GET["x"];
-$y=$_GET["y"];
+$x = $_GET["x"];
+$y = $_GET["y"];
 ?>
 <html>
 <body>
@@ -11,11 +11,29 @@ $y=$_GET["y"];
             <?php for ($i = 0; $i < 11; $i++) { ?>
                 <td>
                     <a href="index.php?x=<?php echo $u ?>&y=<?php echo $i ?>">
-                    <?php if ($u == $x && $i == $y): ?>
-                         <img src="Unbenannt.PNG" style="width: 50px; height: 50px;">
-                    <?php else: ?>
-                        <img src="Download.png" style="width: 50px; height: 50px;">
-                    <?php endif ?>
+                        <?php if ($u == $x && $i == $y): ?>
+                            <img src="Unbenannt.PNG" style="width: 50px; height: 50px;">
+                        <?php else: ?>
+                            <?php if ($u == $x + 1 && $i == $y + 1): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x - 1 && $i == $y + 1): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x - 1 && $i == $y - 1): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x + 1 && $i == $y - 1): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x - 1 && $i == $y): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x + 1 && $i == $y): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x && $i == $y - 1): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php elseif ($u == $x && $i == $y + 1): ?>
+                                <img src="Grüne.png" style="width: 50px; height: 50px;">
+                            <?php else: ?>
+                                <img src="Download.png" style="width: 50px; height: 50px;">
+                            <?php endif ?>
+                        <?php endif ?>
                     </a>
                 </td>
             <?php } ?>
